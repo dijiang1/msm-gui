347aada41cb056ba7c3ad3dbdc728f3e6ead93c7
dijiang1 <djiang10@chicagobooth.edu>
2025-02-12 20:47:58 +0000

    adds a delete link

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
-
+  
   get("/directors/youngest", { :controller => "directors", :action => "max_dob" })
   get("/directors/eldest", { :controller => "directors", :action => "min_dob" })
-
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
-
+  
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
-  post("/insert_movie_record",{ :controller => "movies", :action => "create" })
   
-  get("/actors", { :controller => "actors", :action => "index" })
-  get("/actors/:path_id", { :controller => "actors", :action => "show" })
+  post("/insert_movie_record", { :controller => "movies", :action => "create" })
+  get("/delete_movie/:an_id", { :controller => "movies", :action => "destroy" })
 end
